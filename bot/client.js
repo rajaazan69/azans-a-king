8e1f4190db867a2d517eddc834b5dbcb6382957f
rajaazan69 <azanr7260@gmail.com>
2025-10-17 06:38:44 +0500

    Update client.js

--- bot/client.js
+++ bot/client.js
@@ -13,10 +13,24 @@ const client = new Client({
 client.commands = new Collection();
 
 // Load events
-['ready', 'interactionCreate', 'messageCreate'].forEach(file => {
-    require(`./events/${file}`)(client);
+const eventFiles = ['ready', 'interactionCreate', 'messageCreate'];
+
+eventFiles.forEach(file => {
+    if (file === 'ready') {
+        // ready event only needs client
+        require(`./events/${file}`)(client);
+    } else if (file === 'interactionCreate') {
+        const event = require(`./events/${file}`);
+        // pass client + interaction to your handler
+        client.on('interactionCreate', interaction => event(client, interaction));
+    } else if (file === 'messageCreate') {
+        const event = require(`./events/${file}`);
+        // pass client + message to your handler
+        client.on('messageCreate', message => event(client, message));
+    }
 });
 
+// Login to Discord
 client.login(DISCORD_TOKEN);
 
 module.exports = client;
